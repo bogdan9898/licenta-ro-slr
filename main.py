7d5d0919d88ff8cf1061a0f9f08a2a865e74f4ae
Istoc Bogdan <bogdan.istoc98@gmail.com>
2020-10-11 13:03:41 +0300

    sync

--- main.py
+++ main.py
@@ -118,8 +118,10 @@ if not face_cascade.load(cv2.samples.findFile(args.face_cascade)):
 	exit(0)
 
 cap = cv2.VideoCapture(args.video)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 if not cap.isOpened:
 	print('Video stream cannot be opened')
